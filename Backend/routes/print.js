8a88c89cd63ba582d872af1abb2bb8f764fa92bc
Aadhavan007 <aadhavannamuthukumar@gmail.com>
2026-03-14 11:44:51 +0530

    fix file download from print route

--- Backend/routes/print.js
+++ Backend/routes/print.js
@@ -1,10 +1,4 @@
-const express = require("express")
-const axios = require("axios")
-const jobs = require("../store/jobStore")
-
-const router = express.Router()
-
-router.get("/:job_id", async (req, res) => {
+router.get("/:job_id", (req, res) => {
 
   const jobId = req.params.job_id
   const fileUrl = jobs[jobId]
@@ -13,13 +7,6 @@ router.get("/:job_id", async (req, res) => {
     return res.status(404).send("Job not found")
   }
 
-  const response = await axios.get(fileUrl, { responseType: "stream" })
-
-  res.setHeader("Content-Disposition", "attachment")
-  res.setHeader("Content-Type", response.headers["content-type"])
-
-  response.data.pipe(res)
+  res.redirect(fileUrl)
 
 })
-
-module.exports = router
